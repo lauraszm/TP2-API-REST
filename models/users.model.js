767bce6d1e69ff270ecc354e6ae09c7b6fe7564e
lauraszm <lauraszm@gmail.com>
2024-10-07 20:01:48 -0300

    file system y manejo de error en patch

--- models/users.model.js
+++ models/users.model.js
@@ -1,26 +1,48 @@
-const users = [
-    {id: 1, nombre: "Laura", apellido: "Szmulewicz", email: "laura@mail.com", estudiante: true},
-    {id: 2, nombre: "Pedro", apellido: "Sanchez", email: "pedro@gmail.com", estudiante: true},
-    {id: 3, nombre: "Marta", apellido: "Perez", email: "marta@mail.com", estudiante: false},
-    {id: 4, nombre: "Jose", apellido: "Gimenez", email: "jose@mail.com", estudiante: true},
-    {id: 5, nombre: "Gabriel", apellido: "Hernandez", email: "gabriel@mail.com", estudiante: false},
-]
+import * as fs from 'node:fs';
+
+const usersFromFile = async() => {
+    const allUsers = await fs.promises.readFile('./assets/users.txt', "utf-8")
+    const usersArray = allUsers.split(',\n')
+    let jsonUsers = []
+    usersArray.forEach(item => {
+        if(item !== ''){
+            jsonUsers.push(JSON.parse(item))
+        }
+    })
+    return jsonUsers
+}
 
 const getAllUsers = async() => {
-    return await users;
+   return await usersFromFile()
 }
 
 const postNewUser = async(user) => {
+    const users = await usersFromFile()
     user.id = users[users.length-1].id + 1
     users.push(user)
+    let userString = ""
+    users.forEach(user => {
+        userString += `${JSON.stringify(user)},\n`
+    })
+    const updateFile = await fs.promises.writeFile("./assets/users.txt", userString)
     return user
 }
 
 const patchUser = async(id, user) => {
+    const users = await usersFromFile()
     const userIndex = users.findIndex(us => us.id === parseInt(id))
-    const newUser = {...users[userIndex], ...user}
-    users.splice(userIndex, 1, newUser)
-    return newUser
+    if(userIndex > -1){
+        const newUser = {...users[userIndex], ...user}
+        users.splice(userIndex, 1, newUser)
+        let userString = ""
+        users.forEach(user => {
+            userString += `${JSON.stringify(user)},\n`
+        })
+        const updateFile = await fs.promises.writeFile("./assets/users.txt", userString)
+        return newUser
+    } else {
+        return `No existe usuario con el id ${id}`
+    }
 }
 
 export default {
